b7df07832f2bf1beb5aa076ae6442599703fc74b
pfrueh1 <fruehpaul@gmail.com>
2021-12-29 10:05:37 -0500

    manifest

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
+const WebpackPwaManifest = require("webpack-pwa-manifest");
+const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 
 module.exports = {
     entry: './assets/js/script.js',
@@ -12,6 +14,24 @@ module.exports = {
             $: "jquery",
             jQuery: "jquery"
         }),
+        new BundleAnalyzerPlugin({
+          analyzerMode: 'static'
+        }),
+        new WebpackPwaManifest({
+          name: "Food Event",
+          short_name: "Foodies",
+          description: "An app that allows you to view upcoming food events.",
+          start_url: "../index.html",
+          background_color: "#01579b",
+          theme_color: "#ffffff",
+          fingerprints: false,
+          inject: false,
+          icons: [{
+            src: path.resolve("assets/img/icons/icon-512x512.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          }]
+        })
     ],
     mode: 'development',
     devServer: {
